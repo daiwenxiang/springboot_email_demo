21242b9c6e9d47871f84391f940409af4c983c0c
daiwenxiang <dwx15@hotmail.com>
2020-05-07 22:14:10 +0800

    first commit

---
+++ src/main/java/cn/dwx/rabbitemail/RabbitEmailApplication.java
@@ -0,0 +1,13 @@
+package cn.dwx.rabbitemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitEmailApplication.class, args);
+    }
+
+}
